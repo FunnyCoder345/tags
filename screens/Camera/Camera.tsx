87a0c4a7e5ca5bcb86bf47b2fbeb6ec0dce4e836
FunnyCoder345 <bohdan.dragandev@gmail.com>
2024-05-17 20:33:52 +0300

    added camera without functional

--- screens/Camera/Camera.tsx
+++ screens/Camera/Camera.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert, StyleSheet, View} from 'react-native';
 import {Text, TouchableOpacity} from 'react-native';
+import {
+  Camera,
+  useCameraPermission,
+  useMicrophonePermission,
+  useCameraDevice,
+} from 'react-native-vision-camera';
+const CameraScreen = () => {
+  const {hasPermission, requestPermission} = useCameraPermission(); //Camera permission hook
+  const {
+    hasPermission: hasMicroPermission,
+    requestPermission: requestMicroPermission,
+  } = useMicrophonePermission(); //Microphone permission hook
+  const device = useCameraDevice('back');
+  useEffect(() => {
+    if (!hasPermission) requestPermission(); //request camera permission
+    if (!hasMicroPermission) requestMicroPermission(); //request microphone permission
+  }, [hasPermission, hasMicroPermission]);
 
-const Camera = () => {
-  
+  if (!device) return Alert.alert('Error!', 'Camera not found!');
+
   return (
-    <TouchableOpacity>
-      <Text>Camera</Text>
-    </TouchableOpacity>
+    <Camera style={styles.camera} device={device} isActive={true}></Camera>
   );
 };
 
-export default Camera;
+export default CameraScreen;
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
+});
